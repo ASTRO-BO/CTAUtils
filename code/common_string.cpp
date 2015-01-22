00c7137e58a08adb5f47d9899110e7a01c2df3d5
Andrea Zoli <zoli@iasfbo.inaf.it>
2015-01-22 16:29:04 +0100

    Fix warnings.

--- code/common_string.cpp
+++ code/common_string.cpp
@@ -1,8 +1,8 @@
 #include "common_string.h"
 
-void split(vector<string> &tokens, const string &text, char sep) {
-  unsigned int start = 0, end = 0;
-  while ((end = text.find(sep, start)) != string::npos) {
+void split(std::vector<std::string> &tokens, const std::string &text, char sep) {
+  std::string::size_type start = 0, end = 0;
+  while ((end = text.find(sep, start)) != std::string::npos) {
     tokens.push_back(text.substr(start, end - start));
     start = end + 1;
   }
